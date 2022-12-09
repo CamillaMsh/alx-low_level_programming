04e95e899015e73c9c636b25c32033e4fa2f1a48
CamillaMsh <Camillamshasheni@gmail.com>
2022-12-09 13:36:12 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single numbers from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	int num1;
+
+	for (num1 = 0; num1 < 10; num1++)
+		printf("%d", num1);
+
+	printf("\n");
+
+	return (0);
+
+}
