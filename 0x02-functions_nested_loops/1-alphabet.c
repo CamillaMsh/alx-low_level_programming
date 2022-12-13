50d1b4e97a82fa7a8d959f1520d4a6270a18b92f
CamillaMsh <Camillamshasheni@gmail.com>
2022-12-13 06:15:24 -0800

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets in lower case
+ */
+void print_alphabet(void)
+{
+
+	char lc;
+
+	for (lc  = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+
+}
